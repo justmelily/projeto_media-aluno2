cf48fdd999fca3af46a09f16183f61ccbaa8310f
Aline <aline120@gmail.com>
2024-11-27 12:08:50 -0300

    Adicionando estrutura em python

---
+++ media.py
@@ -0,0 +1,10 @@
+print ("insira as 4 notas")
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+media = (n1 + n2 + n3  ) / 3
+if media > 6 :
+ print ("aprovado")
+elif media < 3:
+ print ("reprovado ")
+else : print ("recuperação")
