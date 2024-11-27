5cbad870f8a271cbb034514a320d73a111042fed
Aline <aline120@gmail.com>
2024-11-27 12:29:25 -0300

    Adicionando pesos

--- media.py
+++ media.py
@@ -2,9 +2,9 @@ print ("insira as 4 notas")
 n1 = float(input())
 n2 = float(input())
 n3 = float(input())
-media = (n1 + n2 + n3  ) / 3
+media = (n1 * 1 + n2 * 1 + n3 * 2  ) / 4
 if media > 6 :
  print ("aprovado")
-elif media < 3:
+elif media < 5:
  print ("reprovado ")
 else : print ("recuperação")
